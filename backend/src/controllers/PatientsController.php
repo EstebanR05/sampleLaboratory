4f8671493f55e9586c6da05131ba42c24232ea58
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2024-08-23 20:16:10 -0500

    se integra sistema de endPoint

--- backend/src/controllers/PatientsController.php
+++ backend/src/controllers/PatientsController.php
@@ -2,8 +2,37 @@
 
 require_once '../services/patients.php';
 
-$data = json_decode(file_get_contents('php://input'), true);
+class PatientsController
+{
+    public function __construct() {}
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($data['name']) && isset($data['birthDate']) && isset($data['address'])) {
-    Patients::createPatients($data['name'], $data['birthDate'], $data['address']);
+    public function handleCreatePatients()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($data['name']) && isset($data['birthDate']) && isset($data['address'])) {
+            Patients::createPatients($data['name'], $data['birthDate'], $data['address']);
+        }
+    }
+}
+
+$controller = new PatientsController();
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        # code...
+        break;
+    case 'POST':
+        $controller->handleCreatePatients();
+        break;
+    case 'PUT':
+        # code...
+        break;
+    case 'DELETE':
+        # code...
+        break;
+    default:
+        http_response_code(405); // Método no permitido
+        echo json_encode(['error' => 'Método no permitido']);
+        break;
 }
